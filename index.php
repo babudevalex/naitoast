04d91df782bccfae4d37a45f3f73658bd5ed8f94
babudevalex <59531079+babudevalex@users.noreply.github.com>
2020-01-20 17:56:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,439 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Nairobi Toastmasters</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Monoton">
+  
+<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+
+<link href='https://fonts.googleapis.com/css?family=Dekko' rel='stylesheet'>
+<link href='w3.css' rel='stylesheet'>
+<style>
+* {
+  box-sizing:border-box;
+}
+
+body {
+font-family: 'Aclonica';
+  word-wrap:break-word;
+   
+}
+.web_font {
+  font-family: 'Aclonica';
+
+}
+
+
+.row {
+  margin: 8px -16px;
+}
+
+/* Add padding BETWEEN each column */
+.row,
+.row > .column {
+  padding: 8px;
+}
+
+/* Create four equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33%;
+}
+
+/* Clear floats after rows */ 
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Content */
+.content {
+
+  padding: 10px;
+  height:300px;
+}
+/* Center website */
+.main {
+  max-width: 1000px;
+  margin: auto;
+  font-family: 'Aclonica';
+  font-size:20px;
+    word-wrap:break-word;
+}
+.pic{
+  width:200px;
+  height:200px;
+}
+
+
+.container {
+  padding: 64px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both
+}
+
+.column-66 {
+  float: left;
+  width: 66.66666%;
+  padding: 20px;
+}
+
+.column-33 {
+  float: left;
+  width: 33.33333%;
+  padding: 20px;
+}
+
+
+.button {
+  border: none;
+  color: white;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #4CAF50;
+}
+
+img {
+  display: block;
+  height: auto;
+  max-width: 100%;
+}
+
+@media screen and (max-width: 1000px) {
+  .column-66,
+  .column-33 {
+    width: 100%;
+    text-align: center;
+  }
+  img {
+    margin: auto;
+  }
+}
+.mySlides {display: none;}
+.w3-bar .w3-button {
+  padding: 36px;
+  }
+  .w3-bar{
+  width:100%;
+  }
+ 
+div.polaroid {
+  width: 284px;
+  padding: 10px 10px 20px 10px;
+  border: 1px solid #BFBFBF;
+  background-color: white;
+  box-shadow: 10px 10px 5px #aaaaaa;
+}
+
+div.rotate_right {
+  float: left;
+  -ms-transform: rotate(7deg); /* IE 9 */
+  -webkit-transform: rotate(7deg); /* Safari */
+  transform: rotate(7deg);
+}
+
+div.rotate_left {
+  float: left;
+  -ms-transform: rotate(-8deg); /* IE 9 */
+  -webkit-transform: rotate(-8deg); /* Safari */
+  transform: rotate(-8deg);
+}
+.grow:hover { transform: scale(1.2); }
+.anchor{
+  -ms-transform: rotate(-2.5deg); /* IE 9 */
+  -webkit-transform: rotate(-2.5deg); /* Safari 3-8 */
+  transform: rotate(-2.5deg);
+
+}
+.anchor:hover{
+  -ms-transform: rotate(0deg); /* IE 9 */
+  -webkit-transform: rotate(0deg); /* Safari 3-8 */
+  transform: rotate(0deg);
+}
+.anchor_menu{
+  -ms-transform: rotate(-7.5deg); /* IE 9 */
+  -webkit-transform: rotate(-7.5deg); /* Safari 3-8 */
+  transform: rotate(-7.5deg);
+
+}
+.anchor_menu:hover{
+  -ms-transform: rotate(0deg); /* IE 9 */
+  -webkit-transform: rotate(0deg); /* Safari 3-8 */
+  transform: rotate(0deg);
+}
+.sameline{
+  background-color: black;
+       width: 100%;
+  text-align:center;
+}
+.glow {
+  font-family: "Monoton", cursive;
+ 
+  font-size: 80px;
+  color:Red;
+  text-align:center;
+  
+}
+
+ .african {
+  font-family: "Monoton", cursive;
+ 
+  font-size: 80px;
+  color:RoyalBlue;
+  text-align:center;
+  
+}
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 900px) {
+  .column {
+    width: 100%;
+	
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .glow {
+    width: 100%;
+	font-size:50px;
+	
+  }
+    .african {
+    width: 100%;
+	font-size:30px;
+	
+  }
+  .sameline{
+      width: 100%;
+  }
+ 
+}
+div.slide-left {
+  width:100%;
+  overflow:hidden;
+
+}
+div.slide-left p {
+  animation: slide-left 2s;
+}
+
+@keyframes slide-left {
+  from {
+    margin-left: 100%;
+    width: 300%; 
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
+}
+@media screen and (max-width: 900px) {
+  .w3-xxlarge{
+	font-size:22px!important;
+	
+  }
+  .w3-xxxlarge{
+	 font-size:24px!important;
+	
+  }
+  .greeting_text{
+       font-size:19px!important;
+	
+  }
+ 
+    
+}
+
+</style>
+</head>
+<body class="web_font">
+<?php include 'menu.php';?>
+
+<!-- image left -->
+<div class="container w3-hover-shadow w3-black">
+  <div class="row">
+    <div class="column-66">
+    
+<p><div style="
+    float: left;
+    color: #2196F3;
+    font-size: 105px;
+    line-height: 60px;
+    padding-top: 13px;
+    padding-right: 8px;
+    padding-left: 3px;
+    font-weight: 100;">W</div><div class="w3-xxlarge greeting_text">elcome to Nairobi Toastmasters Club (NTC), a gathering where sharpening of communication skills
+  and unleashing of members' leadership potential take place. 
+  NTC is one of four toastmasters clubs in Kenya. 
+  It is also the mother club for AAR Toastmasters
+  Club and Early Birds Toastmasters Club.</div></p>
+
+
+    </div>
+    <div class="column-33">
+        <img src="https://res.cloudinary.com/dttubzimi/image/upload/v1571905181/toast/Toastmasters_Inter_Logo.png" width="335" height="471">
+    </div>
+  </div>
+</div>
+<!--end of image left-->
+
+ <div class="w3-container w3-padding-64 w3-center w3-blue w3-hover-shadow" id="art_exhibitons">
+<div class="w3-row">
+ <div class="w3-half ">
+
+ <h2 class="w3-xxxlarge" ><b>Meetings.</b></h2>
+   
+
+   <div class="">
+  <p  class="w3-xxlarge">Our club meets every second and fourth Monday from 12:45 PM to 2:00 PM at United<br> Kenya Club (next to St. Paul's Chapel) off Lower State House Road, Nairobi. Anyone can join Nairobi Toastmasters.</p>
+  <p class="w3-text-blue"><a class="w3-hover-text-red" hreflang="en" href="activities.html">Learn more</a></p>
+         
+         <a href="meetings.php" class="w3-text-hover-green">
+         <button class="button" style="background-color:red">Meetings</button></a>
+  </div>
+  </div>
+   <div class="w3-half ">
+
+<h2 class="w3-xxxlarge" ><b>
+Achievements.</b></h2>
+   
+  
+   <p class="w3-xxlarge">President's Distinguished Club - the highest level of recognition available. The club met the membership prerequisite and achieved at least nine of the 10 goals.</p>
+  <p class="w3-text-blue"><a class="w3-hover-text-red" hreflang="en" href="achievements.html">See more achievements</a></p>	 
+             
+             <a href="achievements.php" class="w3-text-hover-green">
+         <button class="button" style="background-color:red;">Achievements</button></a>
+</div>	
+</div>
+</div>
+
+  <!-- image right -->
+<div class="container w3-hover-shadow w3-white">
+  <div class="row">
+    <div class="column-33">
+      <img src="https://res.cloudinary.com/dttubzimi/image/upload/v1571904500/toast/toastmasters_officials.jpg" alt="App" width="335" height="471">
+    </div>
+    <div class="column-66">
+      <h1 class="w3-xxxlarge" style="color:red;"><b>Officials/Exco..</b></h1>
+      <p class="w3-xxlarge">Get up close with the President, VPE(Vice President Education), VPM(Vice President of Membership), Secretary, VPPR(Vice President of Public Relations), Treasurer and SAA. Find more about their roles in order to get relevant guidance.</p>
+        <a href="officials.php" class="w3-text-hover-green">
+      <button class="button" style="background-color:red">Go to officials.</button></a>
+    </div>
+  </div>
+</div>
+<!-- image left --> 
+
+ <div class="w3-container w3-padding-64 w3-center w3-blue w3-hover-shadow" id="art_exhibitons">
+<div class="w3-row">
+ <div class="w3-half ">
+  <h2 class="w3-xxxlarge"><b>Role Sign Up.</b></h2>
+   
+  
+<p class="w3-xxlarge">Login to select desired role for the next meeting. Sign up to be a Speaker, Evaluator or Toastmaster on Duty.</p>
+  <p class="w3-text-blue"><a class="w3-hover-text-red" hreflang="en" href="contact.html">Quick role sign up.</a></p>
+  
+</div>
+   <div class="w3-half ">
+    <h2 class="w3-xxxlarge"><b>Contact Us.</b></h2>
+   
+ 
+  <p class="w3-xxlarge">Reach out to us for additional assistance. We support email, live chat and other modes.</p>
+  <p class="w3-text-blue"><a class="w3-hover-text-red" hreflang="en" href="contact.html">Contact us/seek help</a></p>
+  
+ </div>
+    </div>
+     </div>
+<!--second row-->
+<div class="w3-container w3-padding-64 w3-center w3-red" >
+<div class="w3-row">
+ <div class="w3-third">
+    <h2 class="w3-xxxlarge"><b>Pay Online.</b></h2>
+   
+ 
+  <p class="w3-xxlarge">Want to be a member immediately without comming to the club? Pay online, receive invoice with link and click the link.</p>
+  <p class="w3-text-yellow w3-xlarge"><a class="w3-hover-text-red" hreflang="en" href="#">Go to payment support</a></p>
+  </div>
+
+  <div class="w3-third">
+
+    <h2 class="w3-xxxlarge"><b>Pathways</b></h2>
+   
+ 
+  <p class="w3-xxlarge">Go to Toastmasters International Pathways. Start Pathways journey and go to base camp. Also access pathways materials. </p>
+  <p class="w3-text-yellow w3-xlarge"><a class="w3-hover-text-red" hreflang="en" href="pathways.php">Go to pathways</a></p>
+  </div>
+
+
+  <div class="w3-third">
+
+    <h2 class="w3-xxxlarge"><b>Forum</b></h2>
+   
+ 
+  <p class="w3-xxlarge">Visit forum and find answers to frequently asked questions. </p>
+  <p class="w3-text-yellow w3-xlarge"><a class="w3-hover-text-red" hreflang="en" href="forum.php">
+  Go to forum</a></p>
+  </div>
+
+ </div>
+</div>
+<?php include 'footer.php';?>
+<script>
+// Slideshow
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+<script>
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+</script>
+</body>
+</html>
